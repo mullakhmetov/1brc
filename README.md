95ad9e405eb618521d280ee664c9552c4addb88e
Artur Mullakhmetov <mullakhmetov.art@gmail.com>
2024-05-12 12:13:05 +0300

    baseline: most naive implementation with linear search by city name

--- README.md
+++ README.md
@@ -13,3 +13,5 @@ The program should print out the min, mean, and max values per station, alphabet
 {Abha=5.0/18.0/27.4, Abidjan=15.7/26.0/34.1, Abéché=12.1/29.4/35.6, Accra=14.7/26.4/33.1, Addis Ababa=2.1/16.0/24.3, Adelaide=4.1/17.3/29.7, ...}
 ```
 
+### Results
+naive linear search (baseline) 7:09.52 total
--- main.c
+++ main.c
@@ -3,8 +3,15 @@
 #include <string.h>
 
 #define MAX_LINE_LENGTH 1 << 10
-#define LIMIT 10 
+#define LIMIT -1
 #define DELIMITER ";"
+
+struct result
+{
+    char city[100];
+    int count;
+    double min, max, sum;
+};
 
 int parse_city(char *str_in, char *city)
 {
@@ -18,16 +25,43 @@ int parse_city(char *str_in, char *city)
     return i;
 }
 
-float parse_temp(int pos, char *str_in, char *temp)
+double parse_temp(int pos, char *str_in)
 {
-    for (int i = 0; i < 5; i++) {
-        temp[i] = str_in[pos + i];
+    char buf[6];
+    int i = 0;
+    while(str_in[i + pos] != '\n') {
+        buf[i] = str_in[i + pos];
+        i++;
     }
-    temp[5] = '\0';
-    int i = pos;
-    while(str_in[i] != 'str_in
+    buf[i] = '\0';
 
-    return atof(temp);
+    return atof(buf);
+}
+
+int get_city_idx(char *city, struct result *results)
+{
+    for (int i = 0; i < 450; i++) {
+        if (strcmp(city, results[i].city) == 0) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+void print_results(struct result results[450], int n_results)
+{
+    for (int i = 0; i < n_results; i++) {
+        printf("%s=%.1f/%.1f/%.1f\n",
+               results[i].city,
+               results[i].min,
+               results[i].sum / results[i].count,
+               results[i].max);
+    }
+}
+
+int results_cmp(const void *a, const void *b) {
+    return strcmp(((struct result*)a)->city, ((struct result*)b)->city);
 }
 
 int main(void)
@@ -38,22 +72,45 @@ int main(void)
         printf("Error opening file!\n");
         return 1;
     }
-   
+    
+    struct result results[450];
+    int n_results = 0;
     long i = 0;
-    char buf[MAX_LINE_LENGTH];
-    while (fgets(buf, MAX_LINE_LENGTH, f_ptr) != NULL && i < LIMIT) {
-        char city[100];
-        int pos = parse_city(buf, city);
 
-        float temp = parse_temp(pos + 1, buf);
-        printf("%s", buf);
-        printf("%s : %f\n", city, temp);
+    char buf[MAX_LINE_LENGTH];
+    while (fgets(buf, MAX_LINE_LENGTH, f_ptr) != NULL && (LIMIT < 0 || i < LIMIT)) {
+        char city[100];
+        int delimiter_pos = parse_city(buf, city);
+        double temp = parse_temp(delimiter_pos + 1, buf);
+
+        int c_idx = get_city_idx(city, results);
+        if (c_idx < 0) {
+            strcpy(results[n_results].city, city);
+            results[n_results].count = 1;
+            results[n_results].min = temp;
+            results[n_results].max = temp;
+            results[n_results].sum = temp;
+
+            n_results++;
+        } else {
+            strcpy(results[c_idx].city, city);
+            results[c_idx].count++;
+            if (temp < results[c_idx].min)
+                results[c_idx].min = temp;
+            if (temp > results[c_idx].max)
+                results[c_idx].max = temp;
+            results[c_idx].sum += temp;
+        }
+        
         i++;
     }
-
-    printf("Successfully read %ld lines the file: %p\n", i, (void *)f_ptr);
-
+    
     fclose(f_ptr);
+
+    qsort(results, (size_t)n_results, sizeof(*results), results_cmp);
+    print_results(results, n_results);
+
+    printf("finished %ld lines\n", i);
 
     return 0;
 }
